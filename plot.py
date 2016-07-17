670589471cac48cd401b34fa4026804d9bd2cab2
abbbsss <bbsahmad@gmail.com>
2016-07-17 19:43:40 +0530

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+import time
+style.use("ggplot")
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1)
+
+
+def animate(i):
+    pullData = open("twitter_out.txt", "r").read()
+    lines = pullData.split('\n')
+
+    xar = []
+    yar = []
+
+    x = 0
+    y = 0
+
+    for l in lines[-200:]:
+        x += 1
+        if "positive" in l:
+            y += 1
+        elif "negetive" in l:
+            y -= 1
+
+        xar.append(x)
+        yar.append(y)
+
+    ax1.clear()
+    ax1.plot(xar, yar)
+
+
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.xlabel("Tweet by time")
+plt.ylabel("Polarity of data")
+plt.title("Sentiment Analysis Result")
+plt.show()
